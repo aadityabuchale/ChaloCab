3f84044a2ba67d5c3f2cae22671a6f6f856a424d
Aaditya Buchale <aadityabuchale15@gmail.com>
2025-03-07 19:54:48 +0530

    updated documentation and minor bug fix

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -2,38 +2,41 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-	fullname: {
-		firstname: {
+const userSchema = new mongoose.Schema(
+	{
+		fullname: {
+			firstname: {
+				type: String,
+				required: true,
+				minlength: [3, "firstname should be atleast 3 characters long"],
+			},
+
+			lastname: {
+				type: String,
+				required: false,
+				minlength: [3, "lastname should be atleast 3 characters long"],
+			},
+		},
+		email: {
 			type: String,
 			required: true,
-			minlength: [3, "firstname should be atleast 3 characters long"],
+			unique: true,
+			minlength: [5, "email should be atleast 5 characters long"],
+		},
+		password: {
+			type: String,
+			required: true,
+			select: false,
+			minlength: [6, "email should be atleast 6 characters long"],
 		},
 
-		lastname: {
+		socketId: {
 			type: String,
 			required: false,
-			minlength: [3, "lastname should be atleast 3 characters long"],
 		},
 	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		minlength: [5, "email should be atleast 5 characters long"],
-	},
-	password: {
-		type: String,
-		required: true,
-		select: false,
-		minlength: [6, "email should be atleast 6 characters long"],
-	},
-
-	socketId: {
-		type: String,
-		required: false,
-	},
-});
+	{ timestamps: true }
+);
 
 userSchema.methods.generateJWTToken = function () {
 	const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
